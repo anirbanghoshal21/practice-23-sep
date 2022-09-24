2b35f09bde16a2ceb7d8c75368693c2ec0ac2b2a
anirbanghoshal21 <111297775+anirbanghoshal21@users.noreply.github.com>
2022-09-24 16:38:31 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ADO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+           string connectionString = "Data Source=BTECH1807169\\SQLEXPRESS;Initial Catalog=pubs;Integrated Security=True";
+           Console.WriteLine("enter the publication id to search the books for:");
+            string pubid =Console.ReadLine();
+            SqlConnection cn = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand("select * from titles where pub_id='" + pubid + "'", cn);
+            cn.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                Console.WriteLine(dr["title_id"]);
+                Console.WriteLine(dr["title"]);
+                Console.WriteLine(dr["type"]);
+                Console.WriteLine(dr["pub_id"]);
+                Console.WriteLine(dr["price"]);
+                Console.WriteLine("----------------------");
+            }
+            cn.Close();
+            Console.Read();
+        }
+    }
+}
